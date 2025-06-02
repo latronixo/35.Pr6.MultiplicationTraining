eba17519d0d57cf633234a29af42ab656bb1735b
Valentin <latronixo@gmail.com>
2025-06-02 19:16:49 +0300

    Initial Commit

---
+++ 35.MultiplicationTraining/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  35.MultiplicationTraining
+//
+//  Created by Валентин on 02.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 35.MultiplicationTraining/_5_MultiplicationTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5_MultiplicationTrainingApp.swift
+//  35.MultiplicationTraining
+//
+//  Created by Валентин on 02.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct _5_MultiplicationTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
