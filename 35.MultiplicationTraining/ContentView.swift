150899f2d84054d7d7ef5873c129181d7cab42ed
Valentin <latronixo@gmail.com>
2025-06-06 17:57:18 +0300

    Сделал структуру Question. Надо разбить макеты на новый представления

--- 35.MultiplicationTraining/ContentView.swift
+++ 35.MultiplicationTraining/ContentView.swift
@@ -7,23 +7,37 @@
 
 import SwiftUI
 
+struct Question {
+    var firstMultiplier: Double
+    var secondMultiplier: Double
+    var answer: Double {
+        return firstMultiplier * secondMultiplier
+    }
+    
+    func getQuestion() -> String {
+        return "Сколько будет \(firstMultiplier) * \(secondMultiplier)?"
+    }
+}
+
 struct ContentView: View {
-    @State private var firstMuliplier = 2.0     //1 множитель
-    private var questionsCount = [5, 10, 20]    //количество вопросов
-    @State private var selectedQuestionsCount = 5   //выбранное количество вопросов
-    @State private var secondMultiplier = [Double]()    //
+    @State private var firstMultiplier = 7.0             //1 множитель
+    private var questionsCount = [5, 10, 20]            //количество вопросов
+    @State private var selectedQuestionsCount = 5       //выбранное количество вопросов
+    @State private var secondMultiplier = [Double]()    //2 множитель
+    @State private var questions = [Question]()
     
     @State private var animationAmount = 0.0    //угол поворота кнопки (для анимации)
-
+    @State private var gameStarted = false      //флаг "игра начата"
+    
     var body: some View {
         VStack {
             Text("Тренировка таблицы умножения")
                 .font(.title)
             Spacer()
             
-            Text("\(firstMuliplier.formatted())")
-                .font(.headline)
-            Stepper("Выберите первый множитель", value: $firstMuliplier, in: 2...13)
+            //Text("\(firstMuliplier.formatted())")
+            //    .font(.headline)
+            Stepper("Первый множитель:          \(firstMultiplier.formatted())", value: $firstMultiplier, in: 2...12)
             
             HStack {
                 Text("Количество вопросов:")
@@ -34,17 +48,18 @@ struct ContentView: View {
                     }
                 }
             }
-            .pickerStyle(.menu)
+            .pickerStyle(.segmented)
             
             Spacer()
-
+            
             Button("Начать") {
                 withAnimation(.spring(duration: 1, bounce: 0.5)) {
                     animationAmount += 360
                 }
+                generateQuestions()
             }
             .padding(50)
-            .background(.red)
+            .background(.indigo)
             .foregroundStyle(.white)
             .clipShape(.circle)
             .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 0, z: 0))
@@ -52,14 +67,25 @@ struct ContentView: View {
         .padding()
     }
     
+    //генерируем новые вопросы:
     func generateQuestions() {
-        //запускаем алерт на 2 секунды
-        Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false) { _ in
         
-            
-            
+        if questions.isEmpty {
+            for i in 0...19 {
+                //secondMultiplier.append(Double(Int.random(in: 2...12)))
+                questions.append(Question(firstMultiplier: firstMultiplier, secondMultiplier: Double(Int.random(in: 2...12)) ))
+                print("secondMultiplier[\(i)] = \(questions[i].secondMultiplier)")
+            }
+        } else {
+            questions.shuffle()
+            print("secondMultiplier = \(questions)")
+        }
+
+        //запускаем таймер на 1 секунду
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { _ in
         }
     }
+    
 }
 
 #Preview {
